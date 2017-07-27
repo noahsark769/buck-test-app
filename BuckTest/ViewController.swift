332d57b158beef0074b15986c27f7556c7fb25b0
Noah Gilmore <noah@yelp.com>
2017-07-27 16:42:29 -0700

    Initial Commit

---
+++ BuckTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuckTest
+//
+//  Created by Noah Gilmore on 7/27/17.
+//  Copyright © 2017 Noah Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
